a901a20265cba626313c63d46573443e8537e479
Martin Ben <mebenav88@gmail.com>
2015-11-28 17:18:49 -0500

    get row from third table

--- extractanchortag.py
+++ extractanchortag.py
@@ -3,6 +3,13 @@ import urllib.request
 import re
 
 html_page = urllib.request.urlopen("https://en.wikipedia.org/wiki/List_of_FIFA_World_Cup_finals")
-soup = BeautifulSoup(html_page)
-for link in soup.findAll('a'):
-    print(link.get('href'))
+# soup = BeautifulSoup(html_page, "html.parser")
+# for link in soup.findAll('a'):
+#     print(link.get('href'))
+
+soup = BeautifulSoup(html_page, "html.parser")
+for link in soup.findAll('table')[2].findAll('tr'):
+    # print(link.get('class'))
+    print(link)
+
+# print(soup.table.tbody.findAll("tr")[1].text)
